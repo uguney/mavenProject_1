7fd9b69b267a69c4c30294822cfac8b07c02952b
uguney <uguney006@gmail.com>
2021-06-10 17:53:20 +0300

    adde more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest");
         System.out.println("developing hello feature");
-
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
